886f1f9dcf7aa4b18a50e8b41772b91419f5cca5
DESKTOP-MARIAM\Admin <mariamtahir105@gmail.com>
2024-04-17 21:40:31 +0500

    Sign Up commit

---
+++ app/src/main/java/com/mariamtahir/smdproj/signUp.kt
@@ -0,0 +1,27 @@
+package com.mariamtahir.smdproj
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class signUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        val signUp = findViewById<Button>(R.id.signUpButton)
+        val login = findViewById<TextView>(R.id.textView)
+
+        signUp.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+
+        login.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+    }
+}
