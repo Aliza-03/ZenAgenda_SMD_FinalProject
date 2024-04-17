24f4c95087844255257cd7a1e84d23dca6b84977
Aliza-03 <alizaibm3000@gmail.com>
2024-04-17 12:20:25 +0500

    Nav attempt between cover and sign up

--- app/src/main/java/com/mariamtahir/smdproj/CoverPage.kt
+++ app/src/main/java/com/mariamtahir/smdproj/CoverPage.kt
@@ -1,11 +1,20 @@
 package com.mariamtahir.smdproj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class CoverPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cover_page)
+        val lg=findViewById<ImageView>(R.id.logo_)
+
+        lg.setOnClickListener {
+            intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/mariamtahir/smdproj/MainActivity.kt
+++ app/src/main/java/com/mariamtahir/smdproj/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.mariamtahir.smdproj
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textclick=findViewById<TextView>(R.id.textclick)
+
+        textclick.setOnClickListener {
+            intent=Intent(this,CoverPage::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
