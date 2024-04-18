acaba3dad4661e19d23fba224857e7f43e14f625
gmustafa.siddiqui18@gmail.com <gmustafa.siddiqui18@gmail.com>
2024-04-18 23:05:42 +0500

    Mustafa committing drawables part2

--- app/src/main/java/com/mariamtahir/smdproj/NotePageactivity.kt
+++ app/src/main/java/com/mariamtahir/smdproj/NotePageactivity.kt
@@ -1,7 +1,12 @@
 package com.mariamtahir.smdproj
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class NotePageactivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notes)
+    }
 
 }
