e447bfd6352d1ea2d97d598996650b69cd9bac4e
Aliza-03 <alizaibm3000@gmail.com>
2024-04-18 23:26:26 +0500

    Adding navigation

--- app/src/main/java/com/mariamtahir/smdproj/taskslist.kt
+++ app/src/main/java/com/mariamtahir/smdproj/taskslist.kt
@@ -3,6 +3,7 @@ package com.mariamtahir.smdproj
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,10 +12,16 @@ class taskslist : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tasks_list)
         val editbutton: ImageButton = findViewById(R.id.editbutton)
-
+        val addtask=findViewById<ImageView>(R.id.add_task_btn)
         editbutton.setOnClickListener {
 
             val intent = Intent(this, EditTaskActivity::class.java)
+            startActivity(intent)
+        }
+
+        addtask.setOnClickListener {
+
+            val intent = Intent(this, AddTask::class.java)
             startActivity(intent)
         }
 
