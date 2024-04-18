552d9c3477c894757a83408cd9fe51b92397df54
Aliza-03 <alizaibm3000@gmail.com>
2024-04-18 16:31:07 +0500

    resources

---
+++ app/src/main/java/com/mariamtahir/smdproj/Notes.kt
@@ -0,0 +1,11 @@
+package com.mariamtahir.smdproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes)
+    }
+}
