abc1bf652fd62125ee2af91586381b38549a90d3
Aliza-03 <alizaibm3000@gmail.com>
2024-04-18 21:08:38 +0500

    Adding navigation

--- app/src/main/java/com/mariamtahir/smdproj/CoverPage.kt
+++ app/src/main/java/com/mariamtahir/smdproj/CoverPage.kt
@@ -14,13 +14,23 @@ class CoverPage : AppCompatActivity() {
         setContentView(R.layout.activity_cover_page)
         val lgin = findViewById<Button>(R.id.loginButton)
         val sign=findViewById<TextView>(R.id.signUp)
+        val forgotpass=findViewById<TextView>(R.id.forgotPassword)
+        val bck=findViewById<ImageView>(R.id.arrowbackLogin)
 
-        lgin.setOnClickListener {
-            intent=Intent(this,
-                profilePage::class.java)
+        bck.setOnClickListener {
+            intent=Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
 
+        forgotpass.setOnClickListener {
+            intent=Intent(this,forgotpass::class.java)
+            startActivity(intent)
+        }
+
+        lgin.setOnClickListener {
+            intent=Intent(this,profilePage::class.java)
+            startActivity(intent)
+        }
         sign.setOnClickListener {
             intent=Intent(this,signUp::class.java)
             startActivity(intent)
--- app/src/main/java/com/mariamtahir/smdproj/signUp.kt
+++ app/src/main/java/com/mariamtahir/smdproj/signUp.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class signUp : AppCompatActivity() {
@@ -14,8 +15,14 @@ class signUp : AppCompatActivity() {
         val signUp = findViewById<Button>(R.id.signUpButton)
         val login = findViewById<TextView>(R.id.login)
 
+        val bck=findViewById<ImageView>(R.id.arrowbackLogin)
+
+        bck.setOnClickListener {
+            intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
         signUp.setOnClickListener {
-            val intent = Intent(this, login::class.java)
+            val intent = Intent(this, profilePage::class.java)
             startActivity(intent)
         }
 
